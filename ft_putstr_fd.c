5970cb8937b18c1c7c9cc6c4a962d31758c497a7
071yoon <yeonggilol@gmail.com>
2021-11-21 15:20:07 +0900

    fixed params

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: 071yoon <071yoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 16:25:22 by 071yoon           #+#    #+#             */
-/*   Updated: 2021/11/13 20:30:06 by 071yoon          ###   ########.fr       */
+/*   Updated: 2021/11/21 15:18:10 by 071yoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	ft_putstr_fd(char *s, int fd)
 	int	i;
 
 	i = 0;
-	if (fd < 0)
+	if (fd < 0 || !s)
 		return ;
-	while (&s[i] && s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		if (ft_isascii(s[i]))
 			ft_putchar_fd(s[i++], fd);
